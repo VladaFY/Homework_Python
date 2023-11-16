a7a6a1dfd93c1f7bdd00a65d00f390b301057b42
Vlada Yuldasheva <VYuldasheva@korusconsulting.ru>
2023-11-16 15:15:58 +0300

    add readmi and modify task by pep8

--- homework/2_base_types/task_02_04.py
+++ homework/2_base_types/task_02_04.py
@@ -1,2 +1,2 @@
-import math 
-print(" GCD of two number 0 and 0 is ", math.gcd(int(input()), int(input()))) 
+import math
+print(" GCD of two number 0 and 0 is ", math.gcd(int(input()), int(input())))
